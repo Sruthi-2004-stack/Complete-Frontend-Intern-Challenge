0e33f60cf5fa02c2ab7218fb500d21f1f04322ae
Sruthi-2004-stack <pullelasruthi86@gmail.com>
2026-06-13 12:18:30 +0530

    Update AnimatedContainer.tsx

--- components/ui/AnimatedContainer.tsx
+++ components/ui/AnimatedContainer.tsx
@@ -1,17 +1,22 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
+import { ReactNode } from "react";
 
-export const containerVariants = {
+interface Props {
+  children: ReactNode;
+}
+
+export const containerVariants: Variants = {
   hidden: {},
   show: {
     transition: {
-      staggerChildren: 0.12,
+      staggerChildren: 0.15,
     },
   },
 };
 
-export const itemVariants = {
+export const itemVariants: Variants = {
   hidden: {
     opacity: 0,
     y: 20,
@@ -21,16 +26,15 @@ export const itemVariants = {
     y: 0,
     transition: {
       type: "spring",
-      stiffness: 120,
+      stiffness: 300,
+      damping: 20,
     },
   },
 };
 
 export default function AnimatedContainer({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+}: Props) {
   return (
     <motion.div
       variants={containerVariants}
